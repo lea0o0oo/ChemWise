b4b1bbd74fd4defdba630f8f27cff247236ed1ea
Leo <le4ndro.fioretti@gmail.com>
2024-05-27 04:50:56 +0200

    f

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -297,7 +297,7 @@ onMounted(() => {
                     >Impostazioni</RouterLink
                   >
                 </li>
-                <li>
+                <li v-if="showCreate">
                   <RouterLink
                     to="/projects"
                     class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
